2b72bdeccb162826c9105d929d4df31adcafc53c
Eduardo Bohrer <nbluisrs@gmail.com>
2012-06-28 00:15:42 -0300

    rails tempalte

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Connectmein::Application.config.secret_token = 'f3eb79b0fee85c6fc5f61b87e0589bd6a7a9ed5e0f43baef9f01160cac2eb5e856d3bb6135d60fb100fe542a5743c30ed677665b99aa4b79f1a691b2169c6702'

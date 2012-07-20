7cab23b0faad436ffc9a008c3e12a61c87482e5f
Eduardo Bohrer <nbluisrs@gmail.com>
2012-07-20 00:52:22 -0300

    using jquery plugin for terminal

--- app/assets/javascripts/console.js
+++ app/assets/javascripts/console.js
@@ -1,2 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function() {
+  $('#console').terminal(function(command, term) {
+    console.log(command);
+  }, {
+    greetings: new Date(),
+    name: 'Terminal',
+    height:600,
+    width:800,
+    prompt:'user@machinename$ '
+  });
+});
